b0ef2c9554e0ad6f7764fcd380cbe980dcb61e10
Arnout Engelen <arnout@bzzt.net>
2023-10-27 18:21:21 +0200

    Port to use libgpiod instead of the RPi-specific libs
    Lightly tested. Edge detection not yet implemented.

--- custom_components/rpi_gpio/__init__.py
+++ custom_components/rpi_gpio/__init__.py
@@ -1,6 +1,9 @@
 """Support for controlling GPIO pins of a Raspberry Pi."""
 
-from RPi import GPIO  # pylint: disable=import-error
+from collections import defaultdict
+
+import gpiod
+import time
 
 from homeassistant.const import (
     EVENT_HOMEASSISTANT_START,
@@ -17,43 +20,64 @@ PLATFORMS = [
     Platform.SWITCH,
 ]
 
+# Using globals like this seems a bit icky, but since GPIO pins
+# are global in very physical sense it might be fine.
+gpiod_config = defaultdict(gpiod.LineSettings)
+gpiod_lines = None
 
 def setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Set up the Raspberry PI GPIO component."""
 
     def cleanup_gpio(event):
         """Stuff to do before stopping."""
-        GPIO.cleanup()
+        global gpiod_config, gpiod_lines
+        gpiod_config.clear()
+        if gpiod_lines:
+            gpiod_lines.close()
 
     def prepare_gpio(event):
         """Stuff to do when Home Assistant starts."""
         hass.bus.listen_once(EVENT_HOMEASSISTANT_STOP, cleanup_gpio)
 
     hass.bus.listen_once(EVENT_HOMEASSISTANT_START, prepare_gpio)
-    GPIO.setmode(GPIO.BCM)
     return True
 
+def update_gpiod_lines():
+    global gpiod_config, gpiod_lines
+
+    if gpiod_lines:
+        gpiod_lines.__exit__()
+
+    gpiod_lines = gpiod.request_lines(
+        "/dev/gpiochip0",
+        consumer="ha-rpi_gpio",
+        config=gpiod_config)
 
 def setup_output(port):
     """Set up a GPIO as output."""
-    GPIO.setup(port, GPIO.OUT)
+    global gpiod_config
+    gpiod_config[port].direction = gpiod.line.Direction.OUTPUT
 
+    update_gpiod_lines()
 
 def setup_input(port, pull_mode):
     """Set up a GPIO as input."""
-    GPIO.setup(port, GPIO.IN, GPIO.PUD_DOWN if pull_mode == "DOWN" else GPIO.PUD_UP)
+    global gpiod_config
+    gpiod_config[port].direction = gpiod.line.Direction.INPUT
+    gpiod_config[port].bias = gpiod.line.Bias.PULL_DOWN if pull_mode == "DOWN" else gpiod.line.Bias.PULL_UP
 
+    update_gpiod_lines()
 
 def write_output(port, value):
     """Write a value to a GPIO."""
-    GPIO.output(port, value)
-
+    global gpiod_lines
+    gpiod_lines.set_value(port, gpiod.line.Value.ACTIVE if value else gpiod.line.Value.INACTIVE)
 
 def read_input(port):
     """Read a value from a GPIO."""
-    return GPIO.input(port)
-
+    global gpiod_lines
+    return gpiod_lines.get_value(port) == gpiod.line.Value.ACTIVE
 
 def edge_detect(port, event_callback, bounce):
     """Add detection for RISING and FALLING events."""
-    GPIO.add_event_detect(port, GPIO.BOTH, callback=event_callback, bouncetime=bounce)
+    # TODO
